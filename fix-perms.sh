8046e2fe01cd11b92abdbee579cc4282c0a94fbf
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2026-06-07 21:57:42 +1200

    Fix UID/GID to 501:20 (macOS), remove permission hack script

--- fix-perms.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Runs in background, fixes permissions on new download folders every 5 seconds
-while true; do
-  find ~/media-server-data/downloads/intermediate -type d ! -perm 777 -exec chmod 777 {} \; 2>/dev/null
-  sleep 5
-done
